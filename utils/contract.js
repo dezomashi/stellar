909858eb6f2ceba3c8220e5f02896599f327c0db
dezomashi <wint76asd@gmail.com>
2026-02-26 23:12:00 +0300

    fix: payable contract

--- utils/contract.js
+++ utils/contract.js
@@ -1,28 +1,20 @@
-import { ethers } from "ethers";
-
-//контракт из Remix
-export const CONTRACT_ADDRESS = "0x48B81B2f981d606e5c29b57b7b85129F8c4Eb46F"; 
-
-export const CONTRACT_ABI = [
-  "function mint(uint256 quantity) public payable",
-  "function totalSupply() view returns (uint256)",
-  "function maxSupply() view returns (uint256)"
-];
-
-export const getContract = (signerOrProvider) => {
-  return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signerOrProvider);
-};
-
 export const mintNFT = async (signer, quantity = 1) => {
-  const contract = getContract(signer);
-  
-  const price = ethers.parseEther("0.05"); 
-  
-  // списание
-  const tx = await contract.mint(quantity, {
-    value: price * BigInt(quantity)
-  });
-  
-  const receipt = await tx.wait();
-  return receipt;
+  try {
+    const contract = getContract(signer);
+    
+    const pricePerNFT = ethers.parseEther("0.05"); 
+    const totalValue = pricePerNFT * BigInt(quantity);
+
+    console.log("Запрос на списание:", ethers.formatEther(totalValue), "ETH");
+
+    
+    const tx = await contract.mint(quantity, {
+      value: totalValue, 
+    });
+
+    return await tx.wait();
+  } catch (error) {
+    console.error("Ошибка транзакции:", error);
+    throw error;
+  }
 };
