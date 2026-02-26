4316733a40d203ecaad2cf432969512e32d4c04f
dezomashi <wint76asd@gmail.com>
2026-02-26 23:19:11 +0300

    resolve ethers v6 provider and minting transaction flow

--- utils/contract.js
+++ utils/contract.js
@@ -5,16 +5,17 @@ export const mintNFT = async (signer, quantity = 1) => {
     const pricePerNFT = ethers.parseEther("0.05"); 
     const totalValue = pricePerNFT * BigInt(quantity);
 
-    console.log("Запрос на списание:", ethers.formatEther(totalValue), "ETH");
+    console.log("Попытка минта. Сумма:", ethers.formatEther(totalValue), "ETH");
 
-    
     const tx = await contract.mint(quantity, {
-      value: totalValue, 
+      value: totalValue
     });
 
+    console.log("Запрос отправлен в MetaMask! Хеш:", tx.hash);
     return await tx.wait();
   } catch (error) {
-    console.error("Ошибка транзакции:", error);
+
+    console.error("Критическая ошибка минта:", error);
     throw error;
   }
 };
